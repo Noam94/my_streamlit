0fbcb86a3e6d43967fd49fc6c69683115da65fff
Noam94 <noam.g79@gmail.com>
2024-06-01 15:12:44 +0300

    my first version

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import seaborn as sns
 import streamlit as st
 import pandas as pd
+
 
 st.write("""
 # My first app
 Hello *world!*
 """)
 
-#df = pd.read_csv("my_data.csv")
-#st.line_chart(df)
+df = sns.load_dataset('dowjones')
+df = df.set_index("Date")
+print(df)
+st.line_chart(df)
